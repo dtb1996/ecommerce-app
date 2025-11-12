7086fa679123ed4169439af033f0ecb3ef630083
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-11-11 22:13:34 -0500

    Updated navbar slide in mobile menu

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -4,13 +4,24 @@ import { FaMagnifyingGlass } from "react-icons/fa6"
 import { LuShoppingCart } from "react-icons/lu"
 import { FaRegUserCircle } from "react-icons/fa"
 import { useCart } from "@/context/CartContext"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FiMenu, FiX } from "react-icons/fi"
 
 export default function Navbar() {
     const { totalItems } = useCart()
     const [menuOpen, setMenuOpen] = useState<boolean>(false)
     const [searchOpen, setSearchOpen] = useState<boolean>(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768 && menuOpen) {
+                setMenuOpen(false)
+            }
+        }
+
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [menuOpen])
 
     return (
         <>
@@ -23,7 +34,7 @@ export default function Navbar() {
                     {menuOpen ? <FiX /> : <FiMenu />}
                 </button>
 
-                <Link to="/" className={styles.companyName}>
+                <Link to="/" className={styles.companyName} onClick={() => setMenuOpen(false)}>
                     RP.SHOP
                 </Link>
 
@@ -43,19 +54,23 @@ export default function Navbar() {
                     <FaMagnifyingGlass className={styles.icon} />
                     <input type="text" placeholder="Search for products..." />
                 </div>
+
                 <div className={styles.linksRight}>
                     <button
                         className={styles.mobileSearch}
-                        onClick={() => setSearchOpen(!menuOpen)}
+                        onClick={() => {
+                            setMenuOpen(false)
+                            setSearchOpen(!searchOpen)
+                        }}
                         aria-label="Toggle search bar"
                     >
                         <FaMagnifyingGlass />
                     </button>
-                    <Link to="/cart" className={styles.cartLink}>
+                    <Link to="/cart" className={styles.cartLink} onClick={() => setMenuOpen(false)}>
                         <LuShoppingCart />
                         {totalItems > 0 && <span className={styles.cartCount}>{totalItems}</span>}
                     </Link>
-                    <Link to="/login">
+                    <Link to="/login" onClick={() => setMenuOpen(false)}>
                         <FaRegUserCircle />
                     </Link>
                 </div>
